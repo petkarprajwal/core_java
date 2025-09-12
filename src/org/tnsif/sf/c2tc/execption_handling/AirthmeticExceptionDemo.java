3552532dc165a64ed02fd3d006c2f1e0f74ef244
Prajwal R Petkar <prajwalrpetkar.22cs@saividya.ac.in>
2025-09-12 20:05:46 +0530

    4th commit

---
+++ src/org/tnsif/sf/c2tc/execption_handling/AirthmeticExceptionDemo.java
@@ -0,0 +1,19 @@
+package org.tnsif.sf.c2tc.execption_handling;
+
+public class AirthmeticExceptionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=50/5;
+		System.out.println(a);
+		int b=50/10;
+		System.out.println(b);
+		int c=50/0;
+		System.out.println(c);
+		int d=50/25;
+		System.out.println(d);
+		
+
+	}
+
+}
---
+++ src/org/tnsif/sf/c2tc/execption_handling/ErrorDemo.java
@@ -0,0 +1,18 @@
+package org.tnsif.sf.c2tc.execption_handling;
+
+public class ErrorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=13;
+		int sum=a+b;
+		
+		System.out.println(sum);
+		
+	}
+	public static int add(int a ,int b) {
+		return a+b;
+	}
+
+}
